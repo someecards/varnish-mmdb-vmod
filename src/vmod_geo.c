5a8b0816fb8faeacb17d4310084054dd990fc6a5
wei001 <wei001@bucknell.edu>
2014-05-28 17:01:08 -0400

    Generalize lookup function to accept an array

--- src/vmod_geo.c
+++ src/vmod_geo.c
@@ -17,7 +17,7 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 }
 
 const char *
-vmod_lookup(struct sess *sp, const char *ipstr, const char *type)
+vmod_lookup(struct sess *sp, const char *ipstr, const char **lookup_path)
 {
         MMDB_s mmdb;
         char *data = NULL;
@@ -56,8 +56,9 @@ vmod_lookup(struct sess *sp, const char *ipstr, const char *type)
         // Parse results
         MMDB_entry_data_s entry_data;
         int exit_code = 0;
+        char* str = NULL;
         if (result.found_entry) {
-            int status = MMDB_get_value(&result.entry, &entry_data, type, "names", "en", NULL);
+            int status = MMDB_aget_value(&result.entry, &entry_data, lookup_path);
 
             if (MMDB_SUCCESS != status) {
                 fprintf(
@@ -71,6 +72,12 @@ vmod_lookup(struct sess *sp, const char *ipstr, const char *type)
                 switch(entry_data.type){
                     case MMDB_DATA_TYPE_UTF8_STRING:
                         data = strndup(entry_data.utf8_string, entry_data.data_size);
+                        break;
+                    case MMDB_DATA_TYPE_UINT16:
+                        str = malloc(entry_data.data_size);
+                        sprintf(str, "%u", entry_data.uint16);
+                        data = strndup(str, entry_data.data_size);
+                        free(str);
                         break;
                     default:
                         fprintf(
@@ -97,12 +104,27 @@ vmod_lookup(struct sess *sp, const char *ipstr, const char *type)
 const char*
 vmod_city(struct sess *sp, const char *ipstr)
 {
-        return vmod_lookup(sp, ipstr, "city");
+        const char *lookup_path[] = {"city", "names", "en", NULL};
+        return vmod_lookup(sp, ipstr, lookup_path);
 }
 
 const char*
 vmod_country(struct sess *sp, const char *ipstr)
 {
-        return vmod_lookup(sp, ipstr, "country");
+        const char *lookup_path[] = {"country", "names", "en", NULL};
+        return vmod_lookup(sp, ipstr, lookup_path);
 }
 
+const char*
+vmod_region_code(struct sess *sp, const char *ipstr)
+{
+        const char *lookup_path[] = {"location", "metro_code", NULL};
+        return vmod_lookup(sp, ipstr, lookup_path);
+}
+
+const char*
+vmod_country_code(struct sess *sp, const char *ipstr)
+{
+        const char *lookup_path[] = {"country", "iso_code", NULL};
+        return vmod_lookup(sp, ipstr, lookup_path);
+}
